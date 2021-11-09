9f6608d1b3180e774dfeb923bf2d12fc240ae829
Stephani Dietz <steph@waya.ai>
2021-11-09 16:19:57 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  let handleClick = () => {
+    setCount(count+1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+    <div class="container">
+    <img src="logo.png" alt=""/>
+    <h1>Comparing JS Frameworks: This one is React</h1>
+    <div class="count">{ count }</div>
+    <button onClick={handleClick}>Increment</button>
+    </div>
     </div>
   );
 }
